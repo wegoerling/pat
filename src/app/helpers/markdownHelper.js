311afadcb81972e41d5947f79a7395af5cb6995e
Jason Foley <jason.s.foley.civ@mail.mil>
2018-11-20 13:35:39 -0500

    Parsing the three single quotes and converting to markdown so Showdown hanldes it properly. Might have some circumstances where it breaks.

--- src/app/helpers/markdownHelper.js
+++ src/app/helpers/markdownHelper.js
@@ -1,14 +1,15 @@
 const showdown = require('showdown');
 var wiky = require('wiky');
+let _ = require('lodash');
 
 exports.convert = function (html) {
     html = html.replace('{{CHECKMARK}}', '&#10063;');
     html = html.replace('{{CHECK MARK}}', '&#10063;');
     html = html.replace('{{CHECK}}', '&#10003;');
+    if (html.includes("'''")) {
+        let regex = /[^A-Za-z0-9]+[\']+[\W]*/gi;
+        html = html.replace(regex, '**');
+    }
     let text = wiky.toHtml(html);
     return new showdown.Converter().makeHtml(html);
 }
-
-// This is for testing. Not too sure how we can incorporate this into application. 
-// Running through each line may be a little to excessive.
-console.log(new showdown.Converter().makeHtml('### **Yo yo**'));
