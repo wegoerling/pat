1adbd3e5c62c83427387062e1809eead26719818
Jason Foley <jason.s.foley.civ@mail.mil>
2018-11-21 07:15:59 -0500

    Cleaning up some code in markdown helper

--- src/app/helpers/markdownHelper.js
+++ src/app/helpers/markdownHelper.js
@@ -1,17 +1,11 @@
 const showdown = require('showdown');
-var wiky = require('wiky');
-let _ = require('lodash');
 
 exports.convert = function (html) {
     html = html.replace('{{CHECKMARK}}', '&#10063;');
     html = html.replace('{{CHECK MARK}}', '&#10063;');
     html = html.replace('{{CHECK}}', '&#10003;');
     if (html.includes("'''")) {
-        console.log(html);
-        let regex = /([\'])+/gi;
-        // Need to place a space around ** on both sides.
-        html = html.replace(regex, "**");
+        html = html.replace(new RegExp(/([\'])+/gi), "**");
     }
-    let text = wiky.toHtml(html);
     return new showdown.Converter().makeHtml(html);
 }
