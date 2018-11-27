2496e3f21f88e358f8e2235468f33c3408daf08c
Foley <1290441117@mil>
2018-11-27 14:31:29 -0500

    Forgot to save changes for * fix.

--- src/app/helpers/markdownHelper.js
+++ src/app/helpers/markdownHelper.js
@@ -13,18 +13,10 @@ exports.convert = function (html) {
     html = html.replace(/{{CHECKMARK}}/gi, '&#10063;');
     html = html.replace(/{{CHECK MARK}}/gi, '&#10063;');
     html = html.replace(/{{CHECK}}/gi, '&#10003;');
-<<<<<<< HEAD
-    if (html.includes("'''")) {
-        console.log(html);
-        let regex = /([\'])+/gi;
-        // Need to place a space around ** on both sides.
-        html = html.replace(regex, '**');
-=======
     if (html.includes("'''") || html.includes('**')) {
         console.log(html);
         let regex = html.includes("'''") ? /([\'])+/gi : /([\*])+/gi;
         html = html.replace(regex, '*');
->>>>>>> jason
     }
     let text = wiky.toHtml(html);
     return new showdown.Converter().makeHtml(text);
