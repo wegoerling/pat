f8b077a40fe593f297e0a758885be3a41fbeb64b
Jose DelaCruz <jodelac@microsoft.com>
2018-11-27 20:52:17 -0500

    merged from remote repo

--- src/app/helpers/markdownHelper.js
+++ src/app/helpers/markdownHelper.js
@@ -13,16 +13,9 @@ exports.convert = function (html) {
     html = html.replace(/{{CHECKMARK}}/gi, '&#10063;');
     html = html.replace(/{{CHECK MARK}}/gi, '&#10063;');
     html = html.replace(/{{CHECK}}/gi, '&#10003;');
-<<<<<<< HEAD
-    if (html.includes("'''")) {
-        let regex = /([\'])+/gi;
-        // Need to place a space around ** on both sides.
-        html = html.replace(regex, '**');
-=======
     if (html.includes("'''") || html.includes('**')) {
         let regex = html.includes("'''") ? /([\'])+/gi : /([\*])+/gi;
         html = html.replace(regex, '*');
->>>>>>> c4b1ec0eb004d6d414f393a243e7b4f2f8b860d0
     }
     let text = wiky.toHtml(html);
     return new showdown.Converter().makeHtml(text);
