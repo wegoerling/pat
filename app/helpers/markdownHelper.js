3955fe3b6ced96bb3ad0a482065219a464e85486
deltj <deltj@outlook.com>
2019-03-16 16:47:13 -0400

    restructured project

---
+++ app/helpers/markdownHelper.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const showdown = require('showdown');
+const wiky = require('wiky');
+
+exports.convert = function (html) {
+    if (!html || html === null || (typeof html !== 'string')) {
+        return html;
+    }
+
+    html = html.replace(/{{CHECKMARK}}/gi, '&#10063;');
+    html = html.replace(/{{CHECK MARK}}/gi, '&#10063;');
+    html = html.replace(/{{CHECK}}/gi, '&#10003;');
+    if (html.includes("'''") || html.includes('**')) {
+        let regex = html.includes("'''") ? /([\'])+/gi : /([\*])+/gi;
+        html = html.replace(regex, '*');
+    }
+    let text = wiky.toHtml(html);
+    return new showdown.Converter().makeHtml(text);
+}
