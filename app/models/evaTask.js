c1fbb5a175d900a869c13443f0bedaa59dde0e6b
deltj <deltj@outlook.com>
2019-03-18 19:28:02 -0400

    removing some commented code

--- app/models/evaTask.js
+++ app/models/evaTask.js
@@ -79,14 +79,3 @@ function taskObjectFromFile(file) {
     return et;
 }
 
-/*
-function DeserializeEvaTaskWithYaml(taskFile, callback) {
-    if (!fs.existsSync(taskFile)) {
-        console.log("\n" + taskFile + "\nFile Does Not Exist\n");
-        callback([]);
-    }
-
-    let yamlFile = YAML.load(taskFile);
-    callback(yamlFile.steps, yamlFile.title, yamlFile.duration);
-}
-*/
