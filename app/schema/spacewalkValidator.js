a1e9a219809e5ce99ac2c1e5a6655e5847d1b745
Christopher Redding <credding@gmail.com>
2019-03-26 21:08:34 -0400

    Started YAML Validation

---
+++ app/schema/spacewalkValidator.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const YamlValidator = require("./yamlValidator");
+
+const PROCEDURE_SCHEMA_FILE = "./app/schema/procedureSchema";
+const TASK_SCHEMA_FILE = "./app/schema/taskSchema";
+
+module.exports = class SpacewalkValidator {
+
+    constructor();
+
+
+    
+    validateProcedureSchema(procedureYamlFilePath) {
+
+        
+
+    }
+
+    validateTaskSchema(taskYamlFilePath) {
+        
+
+
+    }
+
+}
