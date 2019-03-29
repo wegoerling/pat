555baaeea7295eeb25dd697eb7c2f0e76ee5300e
Christopher Redding <credding@gmail.com>
2019-03-28 22:10:45 -0400

    Added tests for ConcurrentStep

---
+++ test/testConcurrentStep.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const _ = require('lodash');
+const path = require('path');
+
+const fs = require('fs');
+const yj = require('yamljs');
+
+const ConcurrentStep = require('../app/model/concurrentStep');
+
+/**
+ * Positive testing for concurrentStep
+ */
+describe('ConcurrentStep constructor - Positive Testing', function() {
+    describe('Normal Input - non-simo', () => {
+        let yamlString = `
+            EV1:
+                - step: "Go Outside"
+        `;
+        var fakeYamlObj = yj.parse(yamlString);
+
+        it('should return a task for normal input', () => {
+
+            let concurrentStep = new ConcurrentStep(fakeYamlObj);
+
+            expect(concurrentStep.EV1).to.exist;
+            expect(concurrentStep.EV1).to.be.an('array');
+            expect(concurrentStep.EV1).to.have.all.keys(0);
+
+            expect(concurrentStep.EV1[0].text).to.be.a('string');
+            expect(concurrentStep.EV1[0].text).to.equal('Go Outside');
+        });
+    });
+
+    describe('Normal Input - simo', () => {
+        let yamlString = `
+            simo: 
+                EV1: "Go Outside"
+                EV2:
+                    - step: "Stay Inside"
+                    - step: "Watch EV1"
+        `;
+        var fakeYamlObj = yj.parse(yamlString);
+
+        it('should return a task for normal input', () => {
+
+            let concurrentStep = new ConcurrentStep(fakeYamlObj);
+
+            expect(concurrentStep.EV1).to.exist;
+            expect(concurrentStep.EV1).to.be.an('array');
+            expect(concurrentStep.EV1).to.have.all.keys(0);
+            expect(concurrentStep.EV1[0].text).to.be.a('string');
+            expect(concurrentStep.EV1[0].text).to.equal('Go Outside');
+
+            expect(concurrentStep.EV2).to.exist;
+            expect(concurrentStep.EV2).to.be.an('array');
+            expect(concurrentStep.EV2).to.have.all.keys(0, 1);
+            expect(concurrentStep.EV2[0].text).to.be.a('string');
+            expect(concurrentStep.EV2[0].text).to.equal('Stay Inside');
+            expect(concurrentStep.EV2[1].text).to.be.a('string');
+            expect(concurrentStep.EV2[1].text).to.equal('Watch EV1');
+        });
+    });
+});
