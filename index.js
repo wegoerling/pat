10dde2ba16571aad0e34bc922f772e53a741823b
deltj <deltj@outlook.com>
2019-04-09 19:33:53 -0400

    fixed default output file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 'use strict';
 
 const program = require('commander');
+const path = require('path');
 
 const app = require('./startup').startup;// calls startup.js?
 const Procedure = require("./app/model/procedure");
@@ -20,6 +21,17 @@ const Procedure = require("./app/model/procedure");
         program.help();
     }
 
+    //  If no output file was specified, use a default
+    if(!program.output) {
+        let p = path.parse(program.input);
+        let file_without_path = p.base;
+        let ext = p.ext;
+        let name = file_without_path.replace(ext, '.html');
+
+        program.output = name;
+
+        console.log('No output filename specified, using default: ' + name);
+    }
 
     // Parse the input file
     let procedure = new Procedure();
