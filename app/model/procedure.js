b403af4bc2dbf84aefee37f0b9a83e843c241b9e
Christopher Redding <credding@gmail.com>
2019-04-19 21:50:36 -0400

    Fixed errors in css reading and template

--- app/model/procedure.js
+++ app/model/procedure.js
@@ -91,7 +91,7 @@ module.exports = class Procedure {
             // Pull in css file if it is defined
             if (procedureYaml.css) {
                 let cssFileName = translatePath(fileName, procedureYaml.css);
-                if (!fs.existsSync(cssFileName) {
+                if (!fs.existsSync(cssFileName)) {
                     throw new Error("Could not find css file " + cssFileName);
                 }
                 this.css = fs.readFileSync(cssFileName);
